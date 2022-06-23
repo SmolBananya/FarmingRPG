71c966113624c3ae204fa9e6e2e617ae3069ca9a
SmolBananya <ronja.kaskela@gmail.com>
2022-06-23 13:58:41 +0300

    Added new enum of item types, item detail class, scriptable object list, furniture sprites.

---
+++ FarmingRPG/Assets/Scripts/Item/ItemDetails.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+
+public class ItemDetails
+{
+    //Acts as an id for item
+    public int itemCode;
+
+    public ItemType itemType;
+    public string itemDescription;
+    public Sprite itemSprite;
+    public string itemLongDescription;
+    public short itemUseGridRadius;
+    public float itemUseRadius;
+    public bool isStartingItem;
+    public bool canBePickedUp;
+    public bool canBeDropped;
+    public bool canBeEaten;
+    public bool canBeCarried;
+}
